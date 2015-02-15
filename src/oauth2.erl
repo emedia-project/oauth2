0c171b4d10215d43407de4a55fc80f50aaaf9106
glejeune <gregoire.lejeune@free.fr>
2015-02-15 23:05:29 +0100

    Update refresh_access_token

--- src/oauth2.erl
+++ src/oauth2.erl
@@ -53,15 +53,26 @@
 -type owner() :: any().
 -type auth() :: map().
 -type response() :: map().
+-type oauth2_error() :: access_denied 
+| invalid_client 
+| invalid_grant 
+| invalid_request 
+| invalid_authorization 
+| invalid_scope 
+| unauthorized_client 
+| unsupported_grant_type 
+| unsupported_response_type 
+| server_error 
+| temporarily_unavailable.
 
 -define(BACKEND, (oauth2_config:backend())).
 -define(TOKEN_GENERATOR, (oauth2_config:token_generator())).
 
--spec authorize_code_request(client_id(), redirect_uri(), user(), scope()) -> {ok, auth()} | {error, any()}.
+-spec authorize_code_request(client_id(), redirect_uri(), user(), scope()) -> {ok, auth()} | {error, oauth2_error()}.
 authorize_code_request(ClientID, RedirectURI, User, Scope) ->
   authorize_code_request(ClientID, RedirectURI, User, Scope, <<>>).
 
--spec authorize_code_request(client_id(), redirect_uri(), user(), scope(), state()) -> {ok, auth()} | {error, any()}.
+-spec authorize_code_request(client_id(), redirect_uri(), user(), scope(), state()) -> {ok, auth()} | {error, oauth2_error()}.
 authorize_code_request(ClientID, RedirectURI, User, Scope, State) -> 
   case ?BACKEND:validate_redirect_uri(ClientID, RedirectURI, State) of
     {ok, State1} -> 
@@ -74,7 +85,7 @@ authorize_code_request(ClientID, RedirectURI, User, Scope, State) ->
                  user => UserInfos,
                  client_id => ClientID,
                  owner => Owner,
-                 scope => Scope1,
+                 scope => oauth2_utils:list_to_scope(Scope1),
                  state => State3,
                  redirect_uri => RedirectURI
                 }};
@@ -88,11 +99,11 @@ authorize_code_request(ClientID, RedirectURI, User, Scope, State) ->
       {error, unauthorized_client}
   end.
 
--spec authorize_code_grant(oauth2:client(), oauth2:redirect_uri(), oauth2:token()) -> {ok, auth()} | {error, any()}.
+-spec authorize_code_grant(oauth2:client(), oauth2:redirect_uri(), oauth2:token()) -> {ok, auth()} | {error, oauth2_error()}.
 authorize_code_grant(Client, RedirectURI, Code) ->
   authorize_code_grant(Client, RedirectURI, Code, <<>>).
   
--spec authorize_code_grant(oauth2:client(), oauth2:redirect_uri(), oauth2:token(), oauth2:state()) -> {ok, auth()} | {error, any()}.
+-spec authorize_code_grant(oauth2:client(), oauth2:redirect_uri(), oauth2:token(), oauth2:state()) -> {ok, auth()} | {error, oauth2_error()}.
 authorize_code_grant({ClientID, _} = Client, RedirectURI, Code, State) ->
   case ?BACKEND:authenticate_client(Client, State) of
     {ok, State1} ->
@@ -101,17 +112,18 @@ authorize_code_grant({ClientID, _} = Client, RedirectURI, Code, State) ->
           Result = ?BACKEND:verify_access_code(Code),
           _ = ?BACKEND:revoke_access_code(Code),
           case Result of
-            {ok, #{client_id := ClientID1, redirect_uri := RedirectURI1, state := State3}} ->
+            {ok, #{client_id := ClientID1, redirect_uri := RedirectURI1, state := State3, associate_token_type := AssociateTokenType}} ->
               case {eutils:compare_as_binary(ClientID, ClientID1),
                     eutils:compare_as_binary(RedirectURI, RedirectURI1),
-                    eutils:compare_as_binary(State2, State3)} of
-                {0, 0, 0} ->
+                    eutils:compare_as_binary(State2, State3),
+                    eutils:compare_as_binary(AssociateTokenType, <<"code">>)} of
+                {0, 0, 0, 0} ->
                   Result;
                 _ ->
-                  {error, unauthorized_client}
+                  {error, invalid_request}
               end;
             _ ->
-              {error, unauthorized_client}
+              {error, invalid_request}
           end;
         {error, _} ->
           _ = ?BACKEND:revoke_access_code(Code),
@@ -122,7 +134,7 @@ authorize_code_grant({ClientID, _} = Client, RedirectURI, Code, State) ->
       {error, unauthorized_client}
   end.
 
--spec implicit_grant(client(), redirect_uri(), user(), scope()) -> {ok, auth()} | {error, any()}.
+-spec implicit_grant(client(), redirect_uri(), user(), scope()) -> {ok, auth()} | {error, oauth2_error()}.
 implicit_grant({ClientID, _} = Client, RedirectURI, User, Scope) -> 
   case ?BACKEND:authenticate_client(Client, <<>>) of
     {ok, _} ->
@@ -137,7 +149,7 @@ implicit_grant({ClientID, _} = Client, RedirectURI, User, Scope) ->
                      user => UserInfos,
                      client_id => ClientID,
                      owner => Owner,
-                     scope => Scope1,
+                     scope => oauth2_utils:list_to_scope(Scope1),
                      state => <<>>,
                      redirect_uri => RedirectURI
                     }};
@@ -154,7 +166,7 @@ implicit_grant({ClientID, _} = Client, RedirectURI, User, Scope) ->
       {error, unauthorized_client}
   end.
 
--spec authorize_client_credentials(client(), scope()) -> {ok, auth()} | {error, any()}.
+-spec authorize_client_credentials(client(), scope()) -> {ok, auth()} | {error, oauth2_error()}.
 authorize_client_credentials({ClientID, _} = Client, Scope) ->
   case ?BACKEND:authenticate_client(Client, <<>>) of
     {ok, _} ->
@@ -163,7 +175,7 @@ authorize_client_credentials({ClientID, _} = Client, Scope) ->
           {ok, #{
              type => client,
              client_id => ClientID,
-             scope => Scope1,
+             scope => oauth2_utils:list_to_scope(Scope1),
              state => <<>>
             }};
         {error, _} ->
@@ -173,22 +185,24 @@ authorize_client_credentials({ClientID, _} = Client, Scope) ->
       {error, unauthorized_client}
   end.
 
--spec issue_code(auth()) -> {ok, response()} | {error, any()}.
+-spec issue_code(auth()) -> {ok, response()} | {error, oauth2_error()}.
 issue_code(#{state := State} = Auth) ->
+  Auth1 = maps:put(associate_token_type, code, Auth),
   case ?BACKEND:associate_access_code(
-          ?TOKEN_GENERATOR:generate(Auth),
-          oauth2_config:expiry(code_grant), Auth) of
+          ?TOKEN_GENERATOR:generate(Auth1),
+          oauth2_config:expiry(code_grant), Auth1) of
     {ok, AccessCode} ->
       {ok, #{code => AccessCode, 
              state => State}};
     E -> E
   end.
 
--spec issue_token(auth()) -> {ok, response()} | {error, any()}.
+-spec issue_token(auth()) -> {ok, response()} | {error, oauth2_error()}.
 issue_token(#{state := State} = Auth) -> 
+  Auth1 = maps:put(associate_token_type, access_token, Auth),
   case ?BACKEND:associate_access_token(
-          ?TOKEN_GENERATOR:generate(Auth),
-          oauth2_config:expiry(access_token), Auth) of
+          ?TOKEN_GENERATOR:generate(Auth1),
+          oauth2_config:expiry(access_token), Auth1) of
     {ok, AccessToken} ->
       {ok, #{access_token => AccessToken,
              expires_in => oauth2_config:expiry(access_token),
@@ -197,15 +211,17 @@ issue_token(#{state := State} = Auth) ->
     E -> E
   end.
 
--spec issue_token_and_refresh(auth()) -> {ok, response()} | {error, any()}.
+-spec issue_token_and_refresh(auth()) -> {ok, response()} | {error, oauth2_error()}.
 issue_token_and_refresh(#{state := State} = Auth) -> 
+  Auth1 = maps:put(associate_token_type, access_token, Auth),
   case ?BACKEND:associate_access_token(
-          ?TOKEN_GENERATOR:generate(Auth),
-          oauth2_config:expiry(access_token), Auth) of
+          ?TOKEN_GENERATOR:generate(Auth1),
+          oauth2_config:expiry(access_token), Auth1) of
     {ok, AccessToken} ->
+      Auth2 = maps:put(associate_token_type, refresh_token, Auth),
       case ?BACKEND:associate_refresh_token(
-              ?TOKEN_GENERATOR:generate(Auth),
-              oauth2_config:expiry(refresh_token), Auth) of
+              ?TOKEN_GENERATOR:generate(Auth2),
+              oauth2_config:expiry(refresh_token), Auth2) of
         {ok, RefreshToken} ->
           {ok, #{access_token => AccessToken,
                  expires_in => oauth2_config:expiry(access_token),
@@ -218,55 +234,58 @@ issue_token_and_refresh(#{state := State} = Auth) ->
     E -> E
   end.
 
--spec verify_access_token(token()) -> {ok, auth()} | {error, any()}.
+-spec verify_access_token(token()) -> {ok, auth()} | {error, oauth2_error()}.
 verify_access_token(Token) -> 
   case ?BACKEND:verify_access_token(Token) of
     {error, E} ->
       _ = ?BACKEND:revoke_access_token(Token),
       {error, E};
-    OK -> OK
+    {ok, #{associate_token_type := AssociateTokenType}} = OK -> 
+      case eutils:compare_as_binary(AssociateTokenType, <<"access_token">>) of
+        0 -> OK;
+        _ -> {error, invalid_request}
+      end
   end.
 
--spec verify_refresh_token(token()) -> {ok, auth()} | {error, any()}.
+-spec verify_refresh_token(token()) -> {ok, auth()} | {error, oauth2_error()}.
 verify_refresh_token(Token) ->
   case ?BACKEND:verify_refresh_token(Token) of
     {error, E} ->
       _ = ?BACKEND:revoke_refresh_token(Token),
       {error, E};
-    OK -> OK
+    {ok, #{associate_token_type := AssociateTokenType}} = OK -> 
+      case eutils:compare_as_binary(AssociateTokenType, <<"refresh_token">>) of
+        0 -> OK;
+        _ -> {error, invalid_request}
+      end
   end.
 
--spec refresh_access_token(token(), token(), scope()) -> {ok, auth()} | {error, any()}.
-refresh_access_token(Token, RefreshToken, Scope) ->
+-spec refresh_access_token(client(), token(), scope()) -> {ok, auth()} | {error, oauth2_error()}.
+refresh_access_token({ClientID, _} = Client, RefreshToken, Scope) ->
   case verify_refresh_token(RefreshToken) of
     {error, _} ->
       {error, invalid_request};
-    {ok, Auth} -> 
-      case verify_access_token(Token) of
-        {ok, #{type := Type} = Auth} ->
-          _ = ?BACKEND:revoke_access_token(Token),
-          _ = ?BACKEND:revoke_refresh_token(RefreshToken),
-          case Type of
-            user ->
-              #{user := UserInfos, owner := Owner, state := State} = Auth,
-              case ?BACKEND:verify_scope(UserInfos, Owner, Scope, State) of
-                {ok, {_, Scope1}} -> 
-                  {ok, maps:put(scope, Scope1, Auth)};
-                {error, _} ->
-                  {error, invalid_scope}
-              end;
-            client ->
-              #{client_id := ClientID} = Auth,
-              case ?BACKEND:verify_client_scope(ClientID, Scope) of
-                {ok, Scope1} ->
-                  {ok, maps:put(scope, Scope1, Auth)};
-                {error, _} ->
-                  {error, invalid_scope}
-              end
+    {ok, #{client_id := ClientID1, scope := InitialScope} = UserAuth} -> 
+      case eutils:compare_as_binary(ClientID1, ClientID) of
+        0 ->
+          Scope1 = if
+                     Scope =:= [] -> oauth2_utils:scope_to_list(InitialScope);
+                     true -> Scope
+                   end,
+          case authorize_client_credentials(Client, Scope1) of
+            {ok, #{client_id := ClientID, scope := Scope2}} ->
+              _ = ?BACKEND:revoke_refresh_token(RefreshToken),
+              UserAuth1 = maps:put(scope, Scope2, UserAuth),
+              UserAuth2 = maps:put(state, <<>>, UserAuth1),
+              {ok, UserAuth2};
+            _ ->
+              {error, unauthorized_client}
           end;
         _ ->
-          {error, invalid_request}
-      end
+          {error, unauthorized_client}
+      end;
+    _ ->
+      {error, unauthorized_client}
   end.
 
 % @hidden
